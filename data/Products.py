88756902f2286263be72c8a7a07698313951653f
Коля <Y98ojun1@yandex.ru>
2020-05-11 23:33:34 +0300

    hgjhjhk GHj hg jhg jhg hhhhhhh

--- data/Products.py
+++ data/Products.py
@@ -1,1 +1,17 @@
-print(123)
+import sqlalchemy
+from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+
+from .db_session import SqlAlchemyBase
+
+
+class Products(SqlAlchemyBase, UserMixin, SerializerMixin):
+    __tablename__ = 'products'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    information = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    photos = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    cost = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    reviews = sqlalchemy.Column(sqlalchemy.String, nullable=True)
