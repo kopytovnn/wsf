3adbeedbc02617f82eb9a855611a428b0f3fe2b0
Коля <Y98ojun1@yandex.ru>
2020-05-12 17:29:17 +0300

    hgjhjhk GHj hg jhg jhg hhhhhhh

--- vk.py
+++ vk.py
@@ -4,15 +4,19 @@ import random
 
 
 def postVK(text, photo):
-    vk_session = vk_api.VkApi(
-        token='19028f9161522857fe5784c36ea96b3e4a54df58a03634d565cb2182b4be8a13e1580c153d0a669548c0f')
+    try:
+        vk_session = vk_api.VkApi(
+            token='19028f9161522857fe5784c36ea96b3e4a54df58a03634d565cb2182b4be8a13e1580c153d0a669548c0f')
 
-    longpoll = VkBotLongPoll(vk_session, "194406021")
-    vk = vk_session.get_api()
-    upload = vk_api.VkUpload(vk_session)
-    photo = upload.photo_wall([photo]
-                              )
+        longpoll = VkBotLongPoll(vk_session, "194406021")
+        vk = vk_session.get_api()
+        upload = vk_api.VkUpload(vk_session)
+        photo = upload.photo_wall([photo]
+                                  )
 
-    vk_photo_id = f"photo{photo[0]['owner_id']}_{photo[0]['id']}"
+        vk_photo_id = f"photo{photo[0]['owner_id']}_{photo[0]['id']}"
 
-    vk.wall.post(message=text, attachments=[vk_photo_id], owner_id="-194406021")
+        vk.wall.post(message=text, attachments=[vk_photo_id], owner_id="-194406021")
+
+    except BaseException:
+        pass
